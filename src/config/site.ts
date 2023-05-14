4f60e74f50877a6e65323b60c4a22e05a94beee6
sadmann7 <sadmann898.gmail.com>
2023-05-14 08:02:52 +0600

    add docs

---
+++ src/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Unstyled Table",
+  description:
+    "An unstyled react table component built on top of @tanstack/react-table v8.",
+  url: "https://unstyled-table.vercel.app/",
+  ogImage: "https://unstyled-table.vercel.app/opengraph-image.png",
+  links: {
+    twitterOne: "https://twitter.com/iamtouha",
+    twitterTwo: "https://twitter.com/sadmann17",
+    github: "https://github.com/otaku-oss/unstyled-table",
+  },
+}

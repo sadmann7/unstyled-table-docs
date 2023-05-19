b721afaa610a69150d9f06849061ad0054aa9606
sadmann7 <sadmann898@gmail.com>
2023-05-20 01:21:10 +0600

    add basic-table codesandbox

--- src/app/docs/examples/basic/page.tsx
+++ src/app/docs/examples/basic/page.tsx
@@ -1,9 +1,15 @@
-import { TableHeaderCell } from "@/components/examples/table-header-cell"
+import { Header } from "@/components/header"
 
-export default function Basic() {
+export default function BasicPage() {
   return (
-    <main className="container grid w-full items-center gap-6 pb-8 pt-6 md:py-10">
-      <TableHeaderCell />
+    <main className="grid w-full items-center gap-6 py-6 lg:py-8">
+      <Header path="Examples/Basic" />
+      <iframe
+        className="w-full rounded-md border"
+        width={896}
+        height={576}
+        src="https://codesandbox.io/p/sandbox/basic-2kvi75?embed=1"
+      ></iframe>
     </main>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,29 @@
+import { Icons } from "./icons"
+
 interface HeaderProps {
-  title: string
+  title?: string
   description?: string
+  showTitle?: boolean
+  path?: string
 }
 
-export function Header({ title, description }: HeaderProps) {
+export function Header({ title, description, path }: HeaderProps) {
   return (
     <div className="grid gap-1">
-      <h1 className="text-3xl font-bold md:text-4xl">{title}</h1>
+      {path ? (
+        <div className="flex items-center space-x-1 text-sm text-muted-foreground">
+          <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+            {path.split("/")[0]}
+          </div>
+          <Icons.chevronRight className="h-4 w-4" aria-hidden="true" />
+          <div className="font-medium text-foreground">
+            {path.split("/")[1]}
+          </div>
+        </div>
+      ) : null}
+      {title ? (
+        <h1 className="text-3xl font-bold md:text-4xl">{title}</h1>
+      ) : null}
       {description ? (
         <p className="text-lg text-muted-foreground">{description}</p>
       ) : null}
